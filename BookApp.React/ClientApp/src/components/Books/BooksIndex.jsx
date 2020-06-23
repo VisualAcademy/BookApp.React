64dd5def768996277fd014d323fb8fd364802afd
RedPlus <redplus@redplus.net>
2020-06-24 04:09:13 +0900

    Promise 기반 HTTP 클라이언트인 axios를 사용하여 Web API 호출하기

--- BookApp.React/ClientApp/src/components/Books/BooksIndex.jsx
+++ BookApp.React/ClientApp/src/components/Books/BooksIndex.jsx
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export class BooksIndex extends Component {
     constructor(props) {
@@ -12,7 +13,9 @@ export class BooksIndex extends Component {
 
     // OnInitialized() 
     componentDidMount() {
-        this.populateBooksData();
+        //this.populateBooksData();
+        //this.populateBooksDataWithAxios();
+        this.populateBooksDataWithAxiosAsync();
     }
 
     // 책 리스트 테이블 출력
@@ -55,9 +58,25 @@ export class BooksIndex extends Component {
         );
     }
 
+    // Fetch API 
     async populateBooksData() {
         const response = await fetch('/api/Books');
         const data = await response.json();
+        this.setState({ books: data, loading: false });
+    }
+
+    // Axios + Sync
+    populateBooksDataWithAxios() {
+        axios.get("/api/Books").then(response => {
+            const data = response.data;
+            this.setState({ books: data, loading: false });
+        });
+    }
+
+    // Axios + Async
+    async populateBooksDataWithAxiosAsync() {
+        const response = await axios.get("/api/Books");
+        const data = response.data;
         this.setState({ books: data, loading: false });
     }
 }
