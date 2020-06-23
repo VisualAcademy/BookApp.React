07ffa41f2a49427696bc38120af790ac04db0bd3
RedPlus <redplus@redplus.net>
2020-06-24 05:10:50 +0900

    Create, Edit, Delete 버튼 및 클릭 이벤트 처리기 생성

--- BookApp.React/ClientApp/src/components/Books/BooksIndex.jsx
+++ BookApp.React/ClientApp/src/components/Books/BooksIndex.jsx
@@ -9,6 +9,9 @@ export class BooksIndex extends Component {
             books: [],
             loading: true
         };
+
+        this.editBy = this.editBy.bind(this);
+        this.deleteBy = this.deleteBy.bind(this); 
     }
 
     // OnInitialized() 
@@ -19,15 +22,16 @@ export class BooksIndex extends Component {
     }
 
     // 책 리스트 테이블 출력
-    static renderBooksTable(books) {
+    renderBooksTable(books) {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
                     <tr>
                         <th>Id</th>
                         <th>Title</th>
-                        <th>-</th>
-                        <th>-</th>
+                        <th>Description</th>
+                        <th>Created</th>
+                        <th>Action, Admin</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -35,8 +39,13 @@ export class BooksIndex extends Component {
                         <tr key={book.id}>
                             <td>{book.id}</td>
                             <td>{book.title}</td>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
+                            <td>{book.description}</td>
+                            <td>{book.created ? new Date(book.created).toLocaleDateString() : "-" }</td>
+                            <td className="text-nowrap">
+                                <button className="btn btn-sm btn-primary" onClick={() => this.editBy(book.id)}>Edit</button>
+                                &nbsp;
+                                <button className="btn btn-sm btn-danger" onClick={() => this.deleteBy(book.id)}>Delete</button>
+                            </td>
                         </tr>
                     )}
                 </tbody>
@@ -44,14 +53,26 @@ export class BooksIndex extends Component {
         );
     }
 
+    goCreatePage() {
+        console.log("Create");
+    }
+
+    editBy(id) {
+        console.log("Edit by " + id);
+    }
+
+    deleteBy(id) {
+        console.log("Delete by " + id);
+    }
+
     render() {
         let contents = this.state.loading
             ? <p><em>Loading....</em></p>
-            : BooksIndex.renderBooksTable(this.state.books);
+            : this.renderBooksTable(this.state.books);
 
         return (
             <div>
-                <h1>My Books</h1>
+                <h1>My Books <button onClick={this.goCreatePage} className="btn btn-primary"><span className="fa fa-plus">+</span></button></h1>
                 <h2>제가 집필한 책입니다.</h2> 
                 {contents}
             </div>
@@ -65,7 +86,7 @@ export class BooksIndex extends Component {
         this.setState({ books: data, loading: false });
     }
 
-    // Axios + Sync
+    // Axios + Async
     populateBooksDataWithAxios() {
         axios.get("/api/Books").then(response => {
             const data = response.data;
