de68829b35a4cc3b1c91c9e140d4e054202fde5e
RedPlus <redplus@redplus.net>
2020-06-24 03:22:34 +0900

    책 리스트를 출력하는 BooksIndex 클래스 컴포넌트 생성 및 기본 모양 테스트

--- BookApp.React/ClientApp/src/App.js
+++ BookApp.React/ClientApp/src/App.js
@@ -7,16 +7,19 @@ import { Counter } from './components/Counter';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+import { BooksIndex } from './components/Books/BooksIndex';
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+                <Route path='/Books' component={BooksIndex} />
+            </Layout>
+        );
+    }
 }
---
+++ BookApp.React/ClientApp/src/components/Books/BooksIndex.jsx
@@ -0,0 +1,52 @@
+﻿import React, { Component } from 'react';
+
+export class BooksIndex extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            books: [],
+            loading: true
+        };
+    }
+
+    // 책 리스트 테이블 출력
+    static renderBooksTable(books) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>-</th>
+                        <th>-</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map(book =>
+                        <tr key={book.id}>
+                            <td>{book.id}</td>
+                            <td>{book.title}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading....</em></p>
+            : BooksIndex.renderBooksTable(this.state.books);
+
+        return (
+            <div>
+                <h1>My Books</h1>
+                <h2>제가 집필한 책입니다.</h2> 
+                {contents}
+            </div>
+        );
+    }
+}
