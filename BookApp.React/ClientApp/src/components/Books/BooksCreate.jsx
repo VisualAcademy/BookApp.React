6d421723c6e51547a462db79fa9362ad19737c47
RedPlus <redplus@redplus.net>
2020-06-25 05:25:14 +0900

    Create 페이지에 대한 UI 작성 및 입력 기능 구현 완료

--- BookApp.React/ClientApp/src/components/Books/BooksCreate.jsx
+++ BookApp.React/ClientApp/src/components/Books/BooksCreate.jsx
@@ -1,15 +1,85 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export class BooksCreate extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            title: '',
+            description: '',
+            created: null,
+        };
 
+        this.handleChangeTitle = this.handleChangeTitle.bind(this); 
+        this.handleChangeDescription = this.handleChangeDescription.bind(this); 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.goIndex = this.goIndex.bind(this); 
+    }
+
+    handleChangeTitle(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    handleChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    goIndex() {
+        const { history } = this.props;
+        history.push("/Books");
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let bookDto = {
+            title: this.state.title,
+            description: this.state.description,
+        };
+
+        axios.post("/api/Books", bookDto).then(result => {
+            this.goIndex();
+        });
     }
 
     render() {
         return (
-            <h1>Create</h1>    
+            <>
+                <h3>Create</h3>
+
+                <div className="row">
+                    <div className="col-md-8">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label>Title</label>
+                                <input type="text" className="form-control"
+                                    placeholder="Enter Title"
+                                    value={this.state.title}
+                                    onChange={this.handleChangeTitle}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Description</label>
+                                <input type="text" className="form-control"
+                                    placeholder="Enter Description"
+                                    value={this.state.description}
+                                    onChange={this.handleChangeDescription}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                                &nbsp;
+                                <button className="btn btn-secondary" onClick={this.goIndex}>List</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </>
         );
     }
 }
